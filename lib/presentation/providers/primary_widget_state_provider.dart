4b0a6eb73d21f6c0e714d45aada046078f2ee5a1
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2024-11-17 08:29:00 +0700

    refactor(providers): update default primary widget and import path

--- lib/presentation/providers/primary_widget_state_provider.dart
+++ lib/presentation/providers/primary_widget_state_provider.dart
@@ -1,10 +1,10 @@
 // lib/presentation/providers/primary_widget_state_provider.dart
 import 'package:flutter/material.dart';
-import 'package:soundify/presentation/widgets/primary/home_widget.dart';
+import 'package:soundify/presentation/screens/app/primary/home/home_screen.dart';
 
 class PrimaryWidgetStateProvider with ChangeNotifier {
-  Widget _activePrimaryWidget =  const HomeWidget(); // Default widget
-  String _primaryWidgetName = 'HomeWidget'; // Nama widget untuk melacak
+  Widget _activePrimaryWidget =  const HomeScreen(); // Default widget
+  String _primaryWidgetName = 'HomeScreen'; // Nama widget untuk melacak
 
   // Getter untuk mendapatkan widget saat ini
   Widget get currentWidget => _activePrimaryWidget;
