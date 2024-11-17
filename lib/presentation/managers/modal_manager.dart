7293cddfa6f88ae964e81af7eca36d437025f23f
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2024-11-17 08:26:10 +0700

    feat(managers): add support for description modals and enhance modal management

--- lib/presentation/managers/modal_manager.dart
+++ lib/presentation/managers/modal_manager.dart
@@ -1,15 +1,46 @@
 // lib/presentation/managers/modal_manager.dart
 import 'package:flutter/material.dart';
 import '../widgets/main/menu_modal.dart';
+import '../widgets/modals/base_modal_overlay.dart';
 
 class ModalManager {
   OverlayEntry? _overlayEntry;
 
-  void showModal(BuildContext context, VoidCallback onClose) {
+  void showMenuModal(BuildContext context, VoidCallback onClose) {
     closeModal();
     
     _overlayEntry = OverlayEntry(
       builder: (context) => MenuModal(onClose: onClose),
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void showDescriptionModal(
+    BuildContext context, {
+    required String title,
+    required String subtitle,
+    required String content,
+    required IconData icon,
+    required Color iconColor,
+    required Color containerColor,
+    double? width,
+    double? height,
+  }) {
+    closeModal();
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => BaseModalOverlay(
+        title: title,
+        subtitle: subtitle,
+        content: content,
+        icon: icon,
+        iconColor: iconColor,
+        containerColor: containerColor,
+        width: width ?? 480,
+        height: height ?? 300,
+        onClose: closeModal,
+      ),
     );
 
     Overlay.of(context).insert(_overlayEntry!);
