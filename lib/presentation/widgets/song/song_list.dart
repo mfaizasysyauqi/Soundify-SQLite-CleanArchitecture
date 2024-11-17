4a931b27796d636b0df8016cecded96ec69de72c
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2024-11-17 08:15:57 +0700

    feat(song): add SongList widget as a placeholder for future implementation

---
+++ lib/presentation/widgets/song/song_list.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/song/song_list.dart
+import 'package:flutter/material.dart';
+
+class SongList extends StatefulWidget {
+  const SongList({super.key});
+
+  @override
+  State<SongList> createState() => _SongListScreenState();
+}
+
+class _SongListScreenState extends State<SongList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
